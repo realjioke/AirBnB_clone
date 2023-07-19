fb8d663fd6a7b6462320e861dfe488e750a0c62b
Your Name <scarlobest@gmail.com>
2023-07-19 08:07:46 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Amenities class that inherits from the baseModel """
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ city subclass of BaseModel """
+
+    name = ""
